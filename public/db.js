146e21a4d916831fb6f27ed33194e5133aee4534
Eiko Ujifusa <eikorouji@gmail.com>
2022-04-13 15:53:14 -0700

    working on db.js

--- public/db.js
+++ public/db.js
@@ -14,3 +14,42 @@ request.onupgradeneeded = event => {
 
 };
 
+function checkDataBase() {
+    const dataBase = request.result;
+
+    // open a transaction on your db //
+    let transaction = db.transaction([pendingObjectStoreName], `readwrite`);
+
+    // access pending object store //
+    let store = transaction.objectStore(pendingObjectStoreName);
+
+    // get all records from store and set to a variable //
+    const getAll = store.getAll();
+
+    // successful get //
+    getAll.onsuccess = () => {
+        if (getAll.result.length > 0) {
+            fetch(`/api/transaction/bulk`, {
+                method: `POST`,
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: `application/json, text/plain, */*`,
+                    "Content-type": `application/json`
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                // success -> open transaction on db //
+
+                // access pending object store //
+
+                // clear store //
+
+            }
+        }
+    }
+}
+
+
+
+window.addEventListener('online', checkDatabase);
